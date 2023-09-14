f8044daba0054a0a55c465e14ae39472e260824b
harryprayiv <harryprayiv@gmail.com>
2023-09-14 14:09:58 -0400

    nix devshell optimization

--- flake.nix
+++ flake.nix
@@ -3,52 +3,45 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.systems.url = "github:nix-systems/default";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    systems,
-  }:
+  outputs = { self, nixpkgs, flake-utils, systems, }:
     flake-utils.lib.eachSystem (import systems)
     (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
+      start = pkgs.writeShellScriptBin "start" ''
+        local settings=$(cat config.yml)
+        echo "current settings are:"
+        echo $settings
+        npm run start -- --cron "*/20 * * * *"
+      '';
+
+      build = pkgs.writeShellApplication {
+        name = "build";
+        runtimeInputs = with pkgs; [nodejs];
+        text = "echo Building! && npm run build";
+      };
+
       name = "Botlt";
-      # systems = [
-      #   "aarch64-darwin"
-      #   "x86_64-darwin"
-      #   "x86_64-linux"
-      # ];
     in {
       devShells.default = pkgs.mkShell {
         inherit name;
+        packages = with pkgs; [
+            build
+            start
+          ];
         buildInputs = [
           pkgs.nodejs
-          # You can set the major version of Node.js to a specific one instead
-          # of the default version
-          # pkgs.nodejs-19_x
-
-          # You can choose pnpm, yarn, or none (npm).
           pkgs.nodePackages.pnpm
-          # pkgs.yarn
-
           pkgs.nodePackages.typescript
           pkgs.nodePackages.typescript-language-server
         ];
+
         shellHook = ''
           export NIX_SHELL_NAME="Botlt"
           echo "Welcome to the devShell!"
-          npm run build
-          echo built
-          echo .
-          echo ..
-          echo ...
-          echo Auto-starting Bot on a 20 min cron...
-          settings=$(cat config.yml)
-          echo current settings are:
-          echo $settings
-          npm run start -- --cron "*/20 * * * *"
+          echo "To run the build function, type: run_build"
+          echo "To start the bot on a 20 min cron, type: run_cron"
         '';
       };
     });
